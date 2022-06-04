3eddac3ed5b524aa4204abdf9488a162ed674377
akshaysen08 <akshaysen.sen75@gmail.com>
2022-06-04 00:11:23 -0400

    React-redux-cart-setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import { Fragment } from 'react';
-import Counter from './components/Counter';
-
-import Header from './components/Header';
-import Auth from './components/Auth'
-import { useSelector } from 'react-redux';
-import UserProfile from './components/UserProfile';
-
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-
-  const authenticated = useSelector(state => state.auth.isAuthenticated)
-
   return (
-    <Fragment>
-      <Header />
-      {!authenticated && <Auth />}
-      {authenticated && <UserProfile />}
-      <Counter />
-    </Fragment>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
